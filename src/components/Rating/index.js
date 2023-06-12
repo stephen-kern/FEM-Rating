6adaecbfac49a3a24796831f3a7ccdee18928558
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-06-12 12:25:07 -0500

    commiting to switch branches

---
+++ src/components/Rating/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Star from '../../../images/icon-star.svg';
+
+
+const Rating = () => {
+    return (  
+        <div className='rating-container'>
+            <img alt='star icon' src={Star}/>
+            
+        </div>
+    );
+}
+ 
+export default Rating;
