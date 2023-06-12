7a57db64b764a2dd108c3033bff596d310d7d1e2
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-06-12 16:16:55 -0500

    more style and slight adjustments to component code. committing to save.

--- src/components/RatingNumbers/index.js
+++ src/components/RatingNumbers/index.js
@@ -4,9 +4,9 @@ const RatingNumbers = ({ rating, handleRatingChange }) => {
   const numbers = [1, 2, 3, 4, 5];
 
   return (
-    <div>
+    <div className="numbers-container">
       {numbers.map((number) => (
-        <button key={number} className={rating >= number ? "active" : ""} onClick={() => handleRatingChange(number)}>
+        <button key={number} className={`rating-button ${rating === number ? "active" : ""}`} onClick={() => handleRatingChange(number)}>
           {number}
         </button>
       ))}
