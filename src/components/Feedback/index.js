e1220227b29435b098df1566049ce63499e11b3f
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-06-12 14:35:53 -0500

    tested to ensure that all functionality is working. user is able to click and submit a rating and updates with the feedback component. Moving onto styling.

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Phone from '../../images/illustration-thank-you.svg'
 
-const Feedback = () => {
+const Feedback = ({ selectedRating }) => {
     return ( 
         <div>
             <img alt='Phone illustration' src={Phone}/>
-            <span>You selected x out of x</span>
+            <span>You selected {selectedRating} out of 5</span>
             <h1>Thank you!</h1>
             <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
         </div>
--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -17,22 +17,23 @@ const Rating = () => {
 
   return (
     <div className="rating-container">
-      <img alt="star icon" src={Star} />
-      <h1>How did we do?</h1>
-      <p>
-        Please let us know how we did with your support request. All feedback is
-        appreciated to help us improve our offering!
-      </p>
       {submitted ? (
-        <Feedback />
+        <Feedback selectedRating={rating} />
       ) : (
         <>
-          {" "}
+          <img alt="star icon" src={Star} />
+          <h1>How did we do?</h1>
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
           <RatingNumbers
             rating={rating}
             handleRatingChange={handleRatingChange}
           />
-          <button type="submit">SUBMIT</button>{" "}
+          <button type="submit" onClick={handleSubmit}>
+            SUBMIT
+          </button>{" "}
         </>
       )}
     </div>
